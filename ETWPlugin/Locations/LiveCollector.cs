bcca24893e9e1190a20a664acfb72432ffb1cdd9
Gus Catalano <guscatalano@users.noreply.github.com>
2025-03-09 20:23:53 -0700

    make an live collector test

--- ETWPlugin/Locations/LiveCollector.cs
+++ ETWPlugin/Locations/LiveCollector.cs
@@ -1,12 +1,128 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using findneedle;
+using Microsoft.Diagnostics.Tracing.Session;
 
 namespace ETWPlugin.Locations;
 
 public class LiveCollector
 {
 
+    private List<string> providersToCollect = new();
+    private TimeSpan timeLimit = TimeSpan.Zero;
+    private int eventLimit = 0;
+    private TraceEventSession? currentSession = null;
+    private Thread? collectorThread = null;
+    private readonly List<ISearchResult> results = [];
+    private readonly ManualResetEvent stopEvent = new(false);
+    private string sessionName = "";
+    private List<string> providersFailedToEnable = [];
+
+    public void Setup(List<string> providersToCollect, TimeSpan? timeLimit = null, int eventLimit = 0, string sessionName = "")
+    {
+        this.providersToCollect = providersToCollect;
+        
+
+        if ((timeLimit == null || timeLimit == TimeSpan.Zero) && eventLimit == 0)
+        {
+            throw new Exception("Time limit or Event limit must be set");
+        }
+        this.timeLimit = timeLimit ?? TimeSpan.Zero;
+        this.eventLimit = eventLimit;
+        this.sessionName = sessionName;
+        if (string.IsNullOrEmpty(this.sessionName))
+        {
+            this.sessionName = "FindNeedle_LiveETW";
+        }
+    }
+
+    public void GetOutputFile()
+    {
+
+    }
+
+    public void StartCollecting()
+    {
+        Thread x = new Thread(CollectorThread);
+        x.Start();
+        collectorThread = x;
+
+        Thread y = new Thread(CounterWatcher);
+        y.Start();
+    }
+
+    private void CounterWatcher()
+    {
+        stopEvent.WaitOne(timeLimit);
+        StopCollecting();
+    }
+
+    private void CollectorThread()
+    {
+        using var KS = new TraceEventSession(sessionName);
+
+        currentSession = KS;
+        foreach (var provider in providersToCollect)
+        {
+            try
+            {
+                KS.EnableProvider(provider);
+            } catch(UnauthorizedAccessException e)
+            {
+                providersFailedToEnable.Add(provider);
+            }
+        }
+        KS.Source.Dynamic.All += Dynamic_All; //Listen to everything
+        KS.Source.Process();
+    }
+
+    public void StopCollecting()
+    {
+        if (currentSession != null)
+        {
+            currentSession.Dispose();
+            currentSession = null;
+        }
+       
+        if(collectorThread != null)
+        {
+            var count = 100;
+            while (collectorThread.IsAlive)
+            {
+                count--;
+                Thread.Sleep(100);
+            }
+            if(count < 0)
+            {
+                throw new Exception("failed to stop collector thread");
+            }
+        }
+    }
+
+    public bool IsCollecting()
+    {
+        return currentSession != null;
+    }
+
+    public List<ISearchResult> GetResultsInMemory()
+    {
+        return results;
+    }
+
+    private void Dynamic_All(Microsoft.Diagnostics.Tracing.TraceEvent obj)
+    {
+        results.Add(new ETLLogLine(obj));
+        if (eventLimit != 0)
+        {
+            if (results.Count() > eventLimit)
+            {
+                stopEvent.Set();
+            }
+        }
+        
+    }
 }
---
+++ ETWPluginTests/LiveETWTests.cs
@@ -0,0 +1,24 @@
+using ETWPlugin.Locations;
+using findneedle;
+
+namespace ETWPluginTests;
+
+[TestClass]
+public class LiveETWTests
+{
+    [TestMethod]
+    public void TestStartAndAutoTimeStop()
+    {
+        
+        LiveCollector collector = new();
+        collector.Setup(new List<string> { "D451642C-63A6-11D7-9720-00B0D03E0347",
+            "DBE9B383-7CF3-4331-91CC-A3CB16A3B538", "E5D3F7AD-54A2-404D-B50B-BF41C40D6CAB", "4846D1C4-912A-4306-B173-24970E40621B" }, TimeSpan.FromSeconds(2), 10);
+        collector.StartCollecting();
+        //collector.StopCollecting();
+        Thread.Sleep(TimeSpan.FromSeconds(5)); //Should auto stop regardless
+        Assert.IsFalse(collector.IsCollecting());
+        List<ISearchResult> results = collector.GetResultsInMemory();
+
+        Assert.IsTrue(results.Count >= 2);
+    }
+}
