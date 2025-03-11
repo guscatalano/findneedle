4859d2d93773a51559cd8e6ba7b379563476ba1c
Gus Catalano <guscatalano@users.noreply.github.com>
2025-03-11 01:42:43 -0700

    add super basic test for the test app so that the method doesnt crash

---
+++ ETWPluginTests/TestLogETWApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETWPluginTests;
+
+[TestClass]
+public class TestLogETWApp
+{
+    [TestMethod]
+    public void BasicTest()
+    {
+        LogETWApp.LogSomeStuff.LogFor10Seconds();
+        Assert.IsTrue(true); //We did not crash so yay
+    }
+}
