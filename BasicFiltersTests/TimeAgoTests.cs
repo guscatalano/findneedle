31153d963611a6b4a872cfa4b41655c7f1ea77a4
Gus Catalano <guscatalano@users.noreply.github.com>
2025-03-14 02:24:16 -0700

    fgh

--- BasicFiltersTests/TimeAgoTests.cs
+++ BasicFiltersTests/TimeAgoTests.cs
@@ -28,7 +28,7 @@ public class FakeSearchResult : ISearchResult
 public sealed class TimeAgoTests
 {
     [TestMethod]
-    public void TimeAgoFilterTestSimple()
+    public void TimeAgoFilterTestSimpleHour()
     {
         var timeAgoFilter = new TimeAgoFilter(TimeAgoUnit.Hour, 1);
         FakeSearchResult result = new();
@@ -36,6 +36,42 @@ public sealed class TimeAgoTests
         Assert.IsTrue(timeAgoFilter.Filter(result));
 
         result.logTime = DateTime.Now.AddMinutes(-90);
+        Assert.IsFalse(timeAgoFilter.Filter(result));
+    }
+
+    [TestMethod]
+    public void TimeAgoFilterTestSimpleDays()
+    {
+        var timeAgoFilter = new TimeAgoFilter(TimeAgoUnit.Day, 1);
+        FakeSearchResult result = new();
+        result.logTime = DateTime.Now.AddHours(-10);
+        Assert.IsTrue(timeAgoFilter.Filter(result));
+
+        result.logTime = DateTime.Now.AddHours(-90);
+        Assert.IsFalse(timeAgoFilter.Filter(result));
+    }
+
+    [TestMethod]
+    public void TimeAgoFilterTestSimpleMinutes()
+    {
+        var timeAgoFilter = new TimeAgoFilter(TimeAgoUnit.Minute, 1);
+        FakeSearchResult result = new();
+        result.logTime = DateTime.Now.AddMinutes(10);
+        Assert.IsTrue(timeAgoFilter.Filter(result));
+
+        result.logTime = DateTime.Now.AddMinutes(-90);
+        Assert.IsFalse(timeAgoFilter.Filter(result));
+    }
+
+    [TestMethod]
+    public void TimeAgoFilterTestSimpleSeconds()
+    {
+        var timeAgoFilter = new TimeAgoFilter(TimeAgoUnit.Second, 1);
+        FakeSearchResult result = new();
+        result.logTime = DateTime.Now.AddSeconds(10);
+        Assert.IsTrue(timeAgoFilter.Filter(result));
+
+        result.logTime = DateTime.Now.AddSeconds(-90);
         Assert.IsFalse(timeAgoFilter.Filter(result));
     }
 }
