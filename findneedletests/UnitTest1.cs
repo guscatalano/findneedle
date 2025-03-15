bc485a5ea288e0f55c9c63acee1222d4fbb50b7c
Gus Catalano <guscatalano@users.noreply.github.com>
2025-03-14 19:20:42 -0700

    fsdfs

--- findneedletests/UnitTest1.cs
+++ findneedletests/UnitTest1.cs
@@ -83,7 +83,7 @@ public class SearchArgsTests
         Assert.AreEqual(3, q.GetLocations().Count);
 
     }
-    
+    /*
     [TestMethod]
     public void TestTimeSpanFilter()
     {
@@ -112,5 +112,5 @@ public class SearchArgsTests
 
         SearchQuery q = new SearchQuery(input);
         Assert.IsTrue(q.GetFilters().Count == 1);
-    }
+    }*/
 }
