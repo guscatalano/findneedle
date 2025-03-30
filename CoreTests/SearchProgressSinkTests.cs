4cfb2b76f5ffd39536fb71246450f5382c1d1918
Gus Catalano <guscatalano@users.noreply.github.com>
2025-03-30 14:33:38 -0700

    added sink notifications

---
+++ CoreTests/SearchProgressSinkTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using findneedle;
+
+namespace CoreTests;
+
+[TestClass]
+public sealed class SearchProgressSinkTests
+{
+    [TestMethod]
+    public void TestBasicSinkNotifications()
+    {
+        SearchProgressSink sink = new();
+        sink.RegisterForTextProgress((string text) => Assert.AreEqual("Test", text));
+        sink.RegisterForNumericProgress((int percent) => Assert.AreEqual(50, percent));
+        sink.NotifyProgress(50, "Test");
+    }
+
+    [TestMethod]
+    public void TestMultipleSinkNotifications()
+    {
+        var tcount = 0;
+        var ncount = 0;
+        SearchProgressSink sink = new();
+        sink.RegisterForTextProgress((string text) => tcount++);
+        sink.RegisterForTextProgress((string text) => tcount++);
+        sink.RegisterForNumericProgress((int percent) => ncount++);
+        sink.RegisterForNumericProgress((int percent) => ncount++);
+
+        sink.NotifyProgress(50, "Test");
+
+        //We subscribed twice, so we should up by 2
+        Assert.AreEqual(tcount, 2);
+        Assert.AreEqual(ncount, 2);
+        sink.NotifyProgress(100, "done");
+        Assert.AreEqual(tcount, 4);
+        Assert.AreEqual(ncount, 4);
+    }
+}
