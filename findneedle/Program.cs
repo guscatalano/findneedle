4af1dca118686b02da0f06781171f972d9b372a7
Gus Catalano <guscatalano@users.noreply.github.com>
2025-04-08 23:42:48 -0700

    switch template so we can exclude this from code coverage

--- findneedle/Program.cs
+++ findneedle/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics.CodeAnalysis;
 using findneedle;
 using findneedle.Implementations;
 using findneedle.Interfaces;
@@ -6,33 +6,42 @@ using findneedle.PluginSubsystem;
 using FindNeedleCoreUtils;
 using FindPluginCore.Searching;
 
-var cancel = false;
-Console.CancelKeyPress += delegate {
-    cancel = true;
-    Console.WriteLine("Cancel received, exiting");
-    Environment.Exit(0);
-};
-
-
-var x = SearchQueryCmdLine.ParseFromCommandLine(Environment.GetCommandLineArgs(), PluginManager.GetSingleton());
-SearchQueryCmdLine.PrintToConsole(x);
-PluginManager.GetSingleton().PrintToConsole();
-Console.WriteLine("If correct, please enter to search, otherwise ctrl-c to exit");
-var input = Console.ReadLine();
-if (!cancel || input != null) //input will be null when its control+c
+[ExcludeFromCodeCoverage]
+internal class Program
 {
-    
-    Console.WriteLine("Searching...");
-    x.LoadAllLocationsInMemory();
-    //var y = x.GetFilteredResults();
+    static void Main(string[] args)
+    {
 
-    x.GetSearchStatsOutput();
+        var cancel = false;
+        Console.CancelKeyPress += delegate {
+            cancel = true;
+            Console.WriteLine("Cancel received, exiting");
+            Environment.Exit(0);
+        };
 
-    x.ProcessAllResultsToOutput();
-    x.PrintOutputFilesToConsole();
 
-    //IResultProcessor p = new WatsonCrashProcessor();
-    //p.ProcessResults(y);
-    //p.GetOutputFile();
+        var x = SearchQueryCmdLine.ParseFromCommandLine(Environment.GetCommandLineArgs(), PluginManager.GetSingleton());
+        SearchQueryCmdLine.PrintToConsole(x);
+        PluginManager.GetSingleton().PrintToConsole();
+        Console.WriteLine("If correct, please enter to search, otherwise ctrl-c to exit");
+        var input = Console.ReadLine();
+        if (!cancel || input != null) //input will be null when its control+c
+        {
+
+            Console.WriteLine("Searching...");
+            x.LoadAllLocationsInMemory();
+            //var y = x.GetFilteredResults();
+
+            x.GetSearchStatsOutput();
+
+            x.ProcessAllResultsToOutput();
+            x.PrintOutputFilesToConsole();
+
+            //IResultProcessor p = new WatsonCrashProcessor();
+            //p.ProcessResults(y);
+            //p.GetOutputFile();
+        }
+
+
+    }
 }
-
