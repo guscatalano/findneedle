e6c373b9837903fc0b25fda0b7e978066666e8c0
Gus Catalano <guscatalano@users.noreply.github.com>
2025-06-17 11:17:06 -0700

    simple error handling

--- FindNeedleUX/Windows/Location/LocationStart.xaml.cs
+++ FindNeedleUX/Windows/Location/LocationStart.xaml.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Threading.Tasks;
 using FindNeedleUX.Services;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using WinUIEx.Messaging;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -19,11 +22,29 @@ public sealed partial class LocationStart : Page
     }
     private void Button_Click(object sender, RoutedEventArgs e)
     {
-        WizardSelectionService.GetCurrentWizard().NavigateNextOne(selectedItem);
+        try
+        {
+            WizardSelectionService.GetCurrentWizard().NavigateNextOne(selectedItem);
+        }
+        catch (Exception)
+        {
+            ShowErrorDialogAsync("Page doesn't exist yet...");
+        }
     }
 
     private void RadioButton_Checked(object sender, RoutedEventArgs e)
     {
         selectedItem = (sender as RadioButton).Name.ToString();
+    }
+    private async Task ShowErrorDialogAsync(string message)
+    {
+        var dialog = new ContentDialog
+        {
+            Title = "Error",
+            Content = message,
+            CloseButtonText = "OK",
+            XamlRoot = this.XamlRoot // Ensure dialog is shown on the correct thread/root
+        };
+        await dialog.ShowAsync();
     }
 }
