b7bf311242ac4e388bf02a03d8a1b7e5d68d5caf
Gus Catalano <guscatalano@users.noreply.github.com>
2026-02-03 00:46:41 -0800

    Update LogETWApp reference and assembly loading in tests
    Removed custom metadata from LogETWApp project reference to make it standard. Updated build to copy LogETWApp binaries to both TestDependencies and output root. Added AssemblyResolve handler in TestLogETWApp to load LogETWApp.dll from TestDependencies, improving test reliability.

--- ETWPluginTests/TestLogETWApp.cs
+++ ETWPluginTests/TestLogETWApp.cs
@@ -3,12 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 
 namespace ETWPluginTests;
 
 [TestClass]
 public class TestLogETWApp
 {
+    static TestLogETWApp()
+    {
+        // Register assembly resolver to find LogETWApp in TestDependencies folder
+        AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+        {
+            if (args.Name.StartsWith("LogETWApp"))
+            {
+                string testDepsPath = Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    "TestDependencies",
+                    "LogETWApp.dll"
+                );
+                if (File.Exists(testDepsPath))
+                {
+                    return Assembly.LoadFrom(testDepsPath);
+                }
+            }
+            return null;
+        };
+    }
+
     [TestMethod]
     public void BasicTest()
     {
@@ -16,3 +39,4 @@ public class TestLogETWApp
         Assert.IsTrue(true); //We did not crash so yay
     }
 }
+
